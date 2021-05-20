9beb73a16df826156ce73fb61747d21e4ff57533
Takashi SASAKI <takashi316@gmail.com>
2021-05-20 16:30:08 +0900

    after the lecture in May 20

---
+++ ListViewSample/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample/MainActivity.kt
@@ -0,0 +1,39 @@
+package jp.ac.kawahara.t_sasaki.listviewsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<ListView>(R.id.lvMenu)
+            .setOnItemClickListener(ListItemClickListener())
+    }
+
+    private inner class ListItemClickListener :
+            AdapterView.OnItemClickListener {
+        override fun onItemClick(p0: AdapterView<*>, p1: View, p2: Int, p3: Long) {
+
+            //AdapterViewにある位置のアイテムの値を問い合わせる
+            //val item = p0.getItemAtPosition(p2) as String
+
+            // p1はTextViewなのでそこから定食名を取り出すこともできる
+            val item = (p1 as TextView).text //toString()しなくてもいいみたい
+
+            val show = "あなたが選んだ定食は " + item
+            Toast.makeText(getApplicationContext() ,show, Toast.LENGTH_LONG).show()
+            // 教科書ではアクティビティそのもの this@MainActivity を第一引数に渡していた
+            // Java なら MainActivity.this と書いていたところ
+        }
+    }
+
+
+
+}
