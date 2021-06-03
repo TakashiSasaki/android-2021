79f0d47e92d221736c14893a3b42a74f51cb3ba2
Takashi SASAKI <takashi316@gmail.com>
2021-06-03 16:09:36 +0900

    6.2.1

---
+++ ConstraintLayoutSample/app/src/main/java/jp/ac/kawahara/t_sasaki/constraintlayoutsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.kawahara.t_sasaki.constraintlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
