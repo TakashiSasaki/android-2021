d9c5d04f15447d829d4cb9b93eff6cb1861e4228
Takashi SASAKI <takashi316@gmail.com>
2021-06-17 15:56:23 +0900

    getStringExtra

--- IntentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/intentsample/MenuThanksActivity.kt
+++ IntentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/intentsample/MenuThanksActivity.kt
@@ -2,10 +2,20 @@ package jp.ac.kawahara.t_sasaki.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
 
 class MenuThanksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_thanks)
+
+        val name = intent?.getStringExtra("menuName")
+        val price = intent?.getStringExtra("menuPrice")
+
+        findViewById<TextView>(R.id.tvMenuName).text = name
+        findViewById<TextView>(R.id.tvMenuPrice).text = price
     }
-}
+
+}//MenuThanksActivity
