ba869cdaa5bc08bbe735616648b6b58b123f8584
Takashi SASAKI <takashi316@gmail.com>
2021-06-17 14:50:03 +0900

    MenuThanksActivity

--- IntentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/intentsample/MenuThanksActivity.kt
+++ IntentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/intentsample/MenuThanksActivity.kt
@@ -3,9 +3,9 @@ package jp.ac.kawahara.t_sasaki.intentsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class MenuThanksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_menu_thanks)
     }
 }
