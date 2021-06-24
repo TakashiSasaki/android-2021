944f7dd6197bca590b92ebee7cc68abd5c8ce034
Takashi SASAKI <takashi316@gmail.com>
2021-06-24 16:12:28 +0900

    8.3

--- MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MenuThanksActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MenuThanksActivity.kt
@@ -2,6 +2,7 @@ package jp.ac.kawahara.t_sasaki.menusample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 
@@ -11,14 +12,25 @@ class MenuThanksActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu_thanks)
 
         val name = intent?.getStringExtra("menuName")
-        val price = intent?.getStringExtra("menuPrice")
+        val price = intent?.getIntExtra("menuPrice", 0)
 
         findViewById<TextView>(R.id.tvMenuName).text = name
-        findViewById<TextView>(R.id.tvMenuPrice).text = price
+        findViewById<TextView>(R.id.tvMenuPrice).text = "" + price
+
+        this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }//onCreate
 
     fun onBackButtonClick(view: View){
         finish()
     }//onBackButtonClick
 
-}
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            finish()
+            return true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }//if
+    }//onOptionsItemSelected
+}//MenuThanksActivity
