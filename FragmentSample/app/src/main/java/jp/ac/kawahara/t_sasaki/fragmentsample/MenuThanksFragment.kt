e8780d59436eb051e47db3685166da848a1c1607
Takashi SASAKI <takashi316@gmail.com>
2021-07-13 14:05:42 +0900

    support xlarge screen size

--- FragmentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/fragmentsample/MenuThanksFragment.kt
+++ FragmentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/fragmentsample/MenuThanksFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
@@ -22,6 +23,7 @@ class MenuThanksFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _isLayoutXLarge = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +37,21 @@ class MenuThanksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val intent = activity?.intent
-        val extras = intent?.extras
+        val frame = activity?.findViewById<FrameLayout>(R.id.menuThanksFrame)
+        if(frame == null) {
+            _isLayoutXLarge = false
+        }
+
+        var extras : Bundle?
+
+        if(_isLayoutXLarge){
+            extras = arguments
+        } else {
+            // Inflate the layout for this fragment
+            val intent = activity?.intent
+            extras = intent?.extras
+        }//if
+
         val menuName = extras?.getString("menuName")
         val menuPrice = extras?.getString("menuPrice")
 
