60c3e1a7860047878b0742fa75c18a2407df83c6
Takashi SASAKI <takashi316@gmail.com>
2021-07-29 13:41:24 +0900

    FragmentSample finished

---
+++ DatabaseSample/app/src/main/java/jp/ac/kawahara/t_sasaki/databasesample/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.kawahara.t_sasaki.databasesample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
