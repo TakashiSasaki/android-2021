abd44775a9abfb81233d7cdf7a8654c07d966b72
Takashi SASAKI <takashi316@gmail.com>
2021-09-02 14:58:03 +0900

    Handler

--- MultiThreadSample/app/src/main/java/jp/ac/kawahara/t_sasaki/multithreadsample/MainActivity.kt
+++ MultiThreadSample/app/src/main/java/jp/ac/kawahara/t_sasaki/multithreadsample/MainActivity.kt
@@ -2,8 +2,11 @@ package jp.ac.kawahara.t_sasaki.multithreadsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -22,29 +25,38 @@ class MainActivity : AppCompatActivity() {
     // 一般論としてのスレッドの概念、AndroidにおけるUIスレッドの概念を理解するために
     // 小規模のアプリを作って動作を確かめてみる。
 
-    private fun funA(){
-        (1..10000).forEach {
+    private fun funA(et:EditText){
+        (1..100).forEach {
             Log.v("funA", "A$it ")
+            Handler(Looper.getMainLooper()).post({
+                et.setText("${et.text} A$it")
+            })
         }//forEach
     }//funA
 
-    private fun funB(){
-        (1..10000).forEach {
+    private fun funB(et:EditText){
+        (1..100).forEach {
             Log.v("funB","B$it ")
+            Handler(Looper.getMainLooper()).post({
+                et.setText("${et.text} B$it")
+            })
         }//forEach
     }//funB
 
     public fun onButtonClick(v: View){
         Log.v("onButtonClick", "start")
+        val et = findViewById<EditText>(R.id.editText)
+
         //ボタンを押すとこの関数がUIスレッドで実行される
-        thread {
-            funA()
+        thread {//Thread A
+            funA(et)
         }
+
         // JavaScriptでは setTimeout(funA,0) でノンブロッキングにできる
         //しかしJSはシングルスレッドなので並列実行にはならない。
 
-        thread {
-            funB()
+        thread {//Thread B
+            funB(et)
         }
         // JavaScriptでは setTimeout(funB, 0) でノンブロッキングにできる
         // しかしJSはシングルスレッドなので並列実行にはならない
